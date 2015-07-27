cc15539cd2d4955f5214f9c6b4c28241fe5b94c5
mattorscheln <mjorsche@gmail.com>
2015-07-26 19:14:13 -0500

    HW5_square_root_COMPLETE

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,25 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+
+  	 @user_number = params["number"]
+  	 @number = @user_number.to_f
+  	 @square = @number * @number 
+    render("square.html.erb")
+  end
+
+  def square_root
+
+  	 @user_number = params["number"]
+  	 @number = @user_number.to_f
+  	 @sqr_rt = @number **0.5
+    render("square_root.html.erb")
+  end
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:number", { :controller => "calculations", :action => "square" })
+
+  get("/square_root/:number", { :controller => "calculations", :action => "square_root" })
+
+
+
 end
